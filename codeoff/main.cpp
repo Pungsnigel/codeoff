c20ee0b18c573fa62784b2b2b4201b37fa59c088
Jesper Persson <jespert.persson@gmail.com>
2014-02-12 09:14:09 +0100

    Initial commit - add project and .gitignore

---
+++ codeoff/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  codeoff
+//
+//  Created by Piggy on 2014-02-12.
+//  Copyright (c) 2014 Piggy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
