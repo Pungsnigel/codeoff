2b13e5b58752325eeeb334e2e737886a640421a3
Jesper Persson <jespert.persson@gmail.com>
2014-02-15 21:25:30 +0100

    Refactor forking from main into exec-function

--- main.cpp
+++ main.cpp
@@ -15,38 +15,24 @@ void startProcess(int *in, int *out, const char *cmd);
 
 int main(int argc, const char * argv[])
 {
-    using namespace std;
     int toFirst[2],  fromFirst[2];
     int toSecond[2], fromSecond[2];
     
     if (argc != 3)      // User didn't specify enough arguments
     {
-        cout << "Usage: " << argv[0] << " cmd1 cmd2"  << endl;
+        std::cout << "Usage: " << argv[0] << " cmd1 cmd2"  << std::endl;
         exit(1);
     }
     
     if (pipe(toFirst) == -1 || pipe(fromFirst) == -1 || pipe(toSecond) == -1 || pipe(fromSecond) == -1)
     {
-        cerr << "Failed to pipe";
+        std::cerr << "Failed to pipe";
         exit(EXIT_FAILURE);
     }
     
-    switch(fork())
-    {
-        case -1: cerr << "Failed to fork";       break;                 // error
-        case 0: startProcess(toFirst, fromFirst, argv[1]); break;       // Child proccess
-        default:                                                        // Main proccess
-        sleep(1);
-            switch(fork())
-            {
-                case -1: cerr << "Failed to fork";          break;  // error
-                case 0: startProcess(toSecond, fromSecond, argv[2]);  break;  // child proccess
-                default:                                                    // Main proccess
-                    sleep(2);
-                    break;
-            }
-            break;
-    }
+    startProcess(toFirst, fromFirst, argv[1]);
+    startProcess(toSecond, fromSecond, argv[2]);
+
     const char *msg = "Hello from Main program! \n";
     char buff [1024];
     std::cout << "Writing to external programs" << std::endl;
@@ -63,10 +49,21 @@ int main(int argc, const char * argv[])
 
 void startProcess(int *in, int *out,const char *cmd)
 {
-    dup2(in[0], 0);
-    dup2(out[1], 1);
-    close(in[1]);    // Child process should not read from input-stream of main
-    close(out[0]);   // Child process should not write from output-stream of main
-    execl(cmd,cmd, (char*) NULL);
+    switch(fork())
+    {
+    case -1:
+        std::cerr << "Failed to fork";
+        break;
+    case 0:
+        dup2(in[0], 0);
+        dup2(out[1], 1);
+        close(in[1]);    // Child process should not read from input-stream of main
+        close(out[0]);   // Child process should not write from output-stream of main
+        execl(cmd,cmd, (char*) NULL);
+        std::cerr << "Something went wrong in excecuting command: " << cmd << std::endl;
+        break;
+    default:
+        return;
+    }
 }
 
