c1a3385f4c58cab455d725d701aff59e94139066
Jesper Persson <jespert.persson@gmail.com>
2014-02-26 13:05:09 +0100

    Remove example prints and program

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int main(int argc, const char * argv[])
     int toFirst[2],  fromFirst[2];
     int toSecond[2], fromSecond[2];
     
-    if (argc != 3)      // User didn't specify enough arguments
+    if (argc != 3)      // User didn't specify enough arguments, exit program.
     {
         std::cout << "Usage: " << argv[0] << " cmd1 cmd2"  << std::endl;
         exit(1);
@@ -33,17 +33,9 @@ int main(int argc, const char * argv[])
     startProcess(toFirst, fromFirst, argv[1]);
     startProcess(toSecond, fromSecond, argv[2]);
 
-    const char *msg = "Hello from Main program! \n";
-    char buff [1024];
-    std::cout << "Writing to external programs" << std::endl;
-    write(toFirst[1], msg, strlen(msg));
-    write(toSecond[1], msg, strlen(msg));
-    sleep(4);
-    read(fromFirst[0], buff, 1024);
-    std::cout << "Main prog read: " << buff << "from first process" << std::endl;
-    read(fromSecond[0], buff, 1024);
-    std::cout << "Main prog read: " << buff << "from second process" << std::endl;
-    std::cout <<"Bertil" << std::endl;
+    /* Piping complete, start main program */
+    
+    
     return 0;
 }
 
@@ -57,8 +49,8 @@ void startProcess(int *in, int *out,const char *cmd)
     case 0:
         dup2(in[0], 0);
         dup2(out[1], 1);
-        close(in[1]);    // Child process should not read from input-stream of main
-        close(out[0]);   // Child process should not write from output-stream of main
+        close(in[1]);    // Close write port for childs input
+        close(out[0]);   // Close read port for chils output
         execl("/bin/sh","sh","-c",cmd,NULL);
         std::cerr << "Something went wrong in excecuting command: " << cmd << std::endl;
         break;
